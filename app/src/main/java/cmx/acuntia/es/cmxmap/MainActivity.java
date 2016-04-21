f9c985bd741a612d2a2d135f03422576ed946e88
gregomartinz <gregomartinz@gmail.com>
2016-04-21 12:44:00 +0200

    Map download working

--- app/src/main/java/cmx/acuntia/es/cmxmap/MainActivity.java
+++ app/src/main/java/cmx/acuntia/es/cmxmap/MainActivity.java
@@ -2,6 +2,9 @@ package cmx.acuntia.es.cmxmap;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +12,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -20,7 +22,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.NetworkInterface;
 import java.net.URL;
 import java.net.URLConnection;
@@ -32,13 +33,17 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final int RED = -65536 ;
     private static TextView text;
-    static ListView lv;
     static ImageView img;
+    static ImageView punto;
+    static Button boton;
 
     static InputStream is = null;
     static JSONObject jObj = null;
+    static JSONObject positionObj = null;
     static JSONArray jarray = null;
+    String ubicacion = "";
     static String json = "";
     String imgMap = "";
 
@@ -51,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button boton = (Button) findViewById(R.id.buttonJson);
+        boton = (Button) findViewById(R.id.buttonJson);
         text = (TextView) findViewById(R.id.textView);
         img = (ImageView) findViewById(R.id.imageView);
 
@@ -64,14 +69,14 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     descarga();
                     downloadMap();
-                } catch (IOException | JSONException e) {
+                } catch (IOException | JSONException | InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
     }
 
-    private void descarga() throws IOException, JSONException {
+    private void descarga() throws IOException, JSONException, InterruptedException {
         // Making HTTP GET
         try {
             String mac = getWifiMacAddress();
@@ -112,11 +117,12 @@ public class MainActivity extends AppCompatActivity {
            Log.e("JSON Parser", "Error parsing data " + e.toString());
         }
         jObj = jarray.getJSONObject(0);
-        JSONObject positionObj = jObj.getJSONObject("mapCoordinate");
+        positionObj = jObj.getJSONObject("mapCoordinate");
         JSONObject aux = jObj.getJSONObject("mapInfo");
         JSONObject aux2 = aux.getJSONObject("image");
         imgMap = aux2.getString("imageName");
-        text.setText(positionObj.toString());
+        getZone();
+        //text.setText(positionObj.toString());
     }
 
     public static String getWifiMacAddress() {
@@ -146,8 +152,8 @@ public class MainActivity extends AppCompatActivity {
         return "";
     }
 
-    private void downloadMap() throws IOException {
-
+    private void downloadMap() throws IOException, JSONException {
+        getJSONData();
         String dir = "http://192.168.104.24/api/config/v1/maps/imagesource/" + imgMap;
         URL url = new URL(dir);
         URLConnection urlConnection = url.openConnection();
@@ -160,8 +166,48 @@ public class MainActivity extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeStream(is);
         img.setImageBitmap(bitmap);
 
-        Log.d("EL mapa está en :" , String.valueOf(img.getX()));
+        Log.d("EL mapa está en :" , String.valueOf(img.getX()) + " " + String.valueOf(img.getY()));
+        Log.d("EL boton está en :" , String.valueOf(boton.getX()));
+    }
+
+    public void getZone() throws JSONException {
+
+        getJSONData();
+        Integer x = positionObj.getInt("x");
+        Integer y = positionObj.getInt("y");
+
+        if(y<60 && x<105 || 45<y && y<55 && x<140){
+            ubicacion = "FORMACION";
+        }if (y>60 && x<135){
+            ubicacion = "PYCLUCAS";
+        }if (y<45 && x>105 && x<140 || x>139 && x<172 && y<55 || x>172 && x<200 && y<61 || x>200 && x<240 && y<75){
+            ubicacion = "SOPORTE";
+        }if (x>139 && x<172 && y>84 || x>172 && x<200 && y>79 || x>200 && x<240 && y>75){
+            ubicacion = "PYCALBERTO";
+        }if (y>55 && y<85 && x>113 && x<160){
+            ubicacion = "HALL";
+        }
+        text.setText(ubicacion);
+        Log.d("El punto en: ", String.valueOf(x));
 
 
+    }
+
+//    @Override
+//    protected void onDraw(Canvas canvas)
+//    {
+//        super.onDraw(canvas);
+//        canvas = new Canvas();
+//        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//        paint.setColor(RED);
+//        canvas.drawCircle(100,50,20,paint);
+//        //canvas.drawPoint(700,y,paint);
+//    }
+
+    private void getJSONData() throws JSONException {
+        positionObj = jObj.getJSONObject("mapCoordinate");
+        JSONObject aux = jObj.getJSONObject("mapInfo");
+        JSONObject aux2 = aux.getJSONObject("image");
+        imgMap = aux2.getString("imageName");
     }
 }
